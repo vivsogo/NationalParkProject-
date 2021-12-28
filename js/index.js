275950a4f6650a74732d708dc29cc70f635b1e43
Vivian Gomez <viviangomez0@gmail.com>
2021-12-27 23:37:38 -0500

    fetch

--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
 alert('yo')
 console.log('please')
 
+// global const since we have to call a few times 
+const baseUrl= 'http://localhost:3000';
+let parks = [];
+
+
+
+
+const loadParks = () => {
+    fetch(baseUrl + '/parks')
+        .then(resp => resp.json())
+        .then(data =>{
+         console.log('data', data)
+             parks = data;
+         })
+
+}
+
+
+ document.addEventListener('DOMContentLoaded',function(){
+    loadParks();
+
+ })
